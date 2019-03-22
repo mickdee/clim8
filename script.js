52c515da57de9841937d86299fac52841d9d2f0f
Mike McDearmon <mmcdearmon@gmail.com>
2019-03-21 21:18:59 -0400

    fix bug where multiple conditions firing at once

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("terminalInput").onkeydown = function(){
   
   // user enters a command
-  if(event.keyCode == 13) {
+  if(event.keyCode === 13) {
         
         // capture the command the user entered and create a container for the response
         let commandEntered = this.value;
@@ -21,10 +21,9 @@ document.getElementById("terminalInput").onkeydown = function(){
                      `;
           document.getElementById("terminalHistory").appendChild(terminalResponseContainer);
           terminalResponseContainer.innerHTML = greeting;
-          document.getElementById('terminalInput').value = ''
-        };
+        }
 
-        if(commandEntered === 'bananas') {
+        else if(commandEntered === 'bananas') {
           console.log('bananas')
         }
 
@@ -33,14 +32,19 @@ document.getElementById("terminalInput").onkeydown = function(){
         // for some reason adding another condition prints BOTH if and else cases
 
         else {
+
+          // user enters a command not understood by clim8
           let commandNotFoundContainer = document.createElement("p");
           let commandNotFoundText = document.createTextNode(this.value + ': command not found');
+          console.log('notfound');
           terminalResponseContainer.appendChild(commandNotFoundContainer);
           document.getElementById("terminalHistory").appendChild(commandNotFoundContainer);
           commandNotFoundContainer.appendChild(commandNotFoundText);
-          document.getElementById('terminalInput').value = ''
         };
         
+        // clear the input after each command is entered
+        document.getElementById('terminalInput').value = ''
+
         // page always stays scrolled to bottom to mimick a terminal window
         window.scrollTo(0,document.body.scrollHeight);
   };
